de58db3d9a0f47627d4cd1052d216cba3e3f6815
Akash Bahai <akashbahai@gmail.com>
2020-08-19 10:42:03 +0200

    Updated input parameters

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import sys
 import score_fasta
 import argparse
-import os
+import os.path
 
 def options():
     try:
@@ -26,7 +26,18 @@ def options():
 
 if __name__ == "__main__":
     inputfile, outputfile, model = options()
-    peptide_list, pred_probability = score_fasta.scoremodel("./input/"+inputfile, "./model/"+model )
+    ifile=''
+    if os.path.isfile('./input/'+inputfile)==True: # check if the inputfile is in the input folder
+        print("Reading inputfile: /input/"+inputfile)
+        ifile = "./input/"+inputfile
+    elif os.path.isfile(inputfile)==True: #check if the inputfile exists in the path
+        print ("Reading inputfile: "+"inputfile")
+        ifile = inputfile
+    else:
+        print ("Error in reading inputfile. Please enter the full path of the inputfile.")
+        sys.exit()
+
+    peptide_list, pred_probability = score_fasta.scoremodel(ifile, "./model/"+model )
     out = open("./output/"+outputfile, 'w')
     for i in range(len(pred_probability)):
         if pred_probability[i][1] >= 0.5:
