8f3e0d24b6c0902eb2825971414675663ae88b0c
Akash Bahai <akashbahai@gmail.com>
2020-11-04 02:31:50 +0100

    updated for multiple fasta files
    added support for multiple fasta files.

--- score_fasta.py
+++ score_fasta.py
@@ -9,7 +9,7 @@ import sys
 import numpy as np
 import os.path
 import pickle
-import pylab as pl
+#import pylab as pl
 from scipy import interp
 
 protein = PyPro()
@@ -21,6 +21,8 @@ class MyCustomUnpickler(pickle.Unpickler):
         return super().find_class(module, name)
 
 
+def error(err):
+    sys.exit(err)
 
 
 def readAAP(file):  #read AAP features from the AAP textfile
@@ -32,8 +34,7 @@ def readAAP(file):  #read AAP features from the AAP textfile
         aapdata.close()
         return aapdic
     except:
-        print("Error in reading AAP feature file. Please make sure that the AAP file is correctly formatted")
-        sys.exit()
+        error("Error in reading AAP feature file. Please make sure that the AAP file is correctly formatted")
 
 
 def readAAT(file):  #read AAT features from the AAT textfile
@@ -45,8 +46,7 @@ def readAAT(file):  #read AAT features from the AAT textfile
         aatdata.close()
         return aatdic
     except:
-        print("Error in reading AAT feature file. Please make sure that the AAT file is correctly formatted")
-        sys.exit()
+        error("Error in reading AAT feature file. Please make sure that the AAT file is correctly formatted")
 
 
 def aap(pep, aapdic, avg):  #return AAP features for the peptides
@@ -164,6 +164,7 @@ def protvec(pep, k, file): #Calculate ProtVec representation
     feature = SequenceKmerEmbRep(file, pep, 'protein', k)
     return feature
     
+
 def PAAC(pep):
     feature = []
     for seq in pep:
@@ -175,39 +176,104 @@ def PAAC(pep):
     return feature
 
 
-def readseq(file):  #read the sequence from the fasta file
-    try:
-        sequence = SeqIO.read(file, "fasta")
-        print("Amino acid sequence:",sequence.seq)
-        for i in sequence.seq:
-            #print(i)
-            if i.upper() in ['A', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'V', 'W', 'Y'] :
-                continue
-            else:
-                print("\'"+i+"\' is not a valid amino acid. Please enter sequences with only 20 aa code.")
-                sys.exit()
-        return(str(sequence.seq))
-    except ValueError:
-        print("Please enter a valid fasta file")
-        sys.exit()
-
-
-def peptides(seq):  #return peptides of length 20 from the sequence
-    pep = []
-    i=0
-    while i < len(seq):
-        if len(seq)<20:
-            pep.append(seq)
-            break
-        if len(seq)>=20:
-            if i+20 > len(seq):
-                break
-            else:
-                pep.append(seq[i:i+20])
-            i = i + 1
-    #print(pep)
-    return pep
+def combinefeature(pep, featurelist, vocab, aap_file, aat_file):
     
+    a=np.empty([len(pep), 1])
+    if 'aap' in featurelist:
+        aapdic = readAAP("./aap/"+aap_file)
+        f_aap = np.array([aap(pep, aapdic, 1)]).T
+        a = np.column_stack((a,f_aap))
+        #print(f_aap)
+    if 'aat' in featurelist:
+        aatdic = readAAT("./aat/"+aat_file)
+        f_aat = np.array([aat(pep, aatdic, 1)]).T
+        a = np.column_stack((a, f_aat))
+        #print(f_aat)
+    if 'dpc' in featurelist:
+        f_dpc, name = DPC(pep)
+        # f_dpc = np.average(f_dpc, axis =1)
+        a = np.column_stack((a, np.array(f_dpc)))
+    if 'aac' in featurelist:
+        f_aac = AAC(pep)
+        a = np.column_stack((a, np.array(f_aac)))
+        #fname = fname + name
+    if 'paac' in featurelist:
+        f_paac = PAAC(pep)
+        f_paac = pca.fit_transform(f_paac)
+        a = np.column_stack((a, np.array(f_paac)))
+        #fname = fname + name
+    if 'kmer' in featurelist:
+        kmers = kmer(pep, 4, vocab=vocab, testing=1)
+        #f_kmer = np.array(kmers.X.toarray())
+        f_kmer = np.array(kmers.X.toarray())
+        a = np.column_stack((a, f_kmer))
+        #fname = fname + name
+    if 'ctd' in featurelist:
+        f_ctd, name = CTD(pep)
+        a = np.column_stack((a, np.array(f_ctd)))
+        #fname = fname + name
+    if 'protvec' in featurelist:
+        f_protvec = np.array(protvec(pep, 4, './protvec/sp_sequences_4mers_vec.bin').embeddingX)
+        a = np.column_stack((a, f_protvec))
+            
+    #print(a)
+    return a[:,1:]
+
+
+def readseq(file, length):  #read the sequences from the fasta file
+    print("=================================================================\n")
+    try:
+        records = list(SeqIO.parse(file, "fasta"))
+        # print("Amino acid sequence:",sequence.seq)
+        sequences={}
+        
+        if len(records) > 1:
+            print("Found "+str(len(records))+" protein sequences\n")
+        else:
+            print("Found "+str(len(records))+" protein sequence\n")
+        
+        maxlenname=0
+        maxlenseq=0
+        for i in records:
+            if len(str(i.id)) > maxlenname:
+                maxlenname = len(str(i.id))
+            if len(str(i.seq)) > maxlenseq:
+                maxlenseq = len(str(i.seq))
+        
+        for i in records:
+            #print("test")
+            seq_check = 0
+            len_check = 0
+            if (len(str(i.seq)) >=4):
+                len_check = 1
+            if (len(str(i.seq)) <4):
+                len_check = 0
+                print(i.id.ljust(maxlenname,' '),":",i.seq)
+                print("Error in "+i.id+" sequence.The length of the sequence can't be less than 4. This sequence will be skipped. \n", file=sys.stderr)
+                if(len(records))==1:
+                        error("Error. No valid sequence found")
+                continue
+            for j in i.seq:
+                if j.upper() in ['A', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'V', 'W', 'Y'] :
+                    seq_check = 1
+                else:
+                    seq_check = 0
+                    print(i.id.ljust(maxlenname,' '),":",i.seq)
+                    print("Error in "+i.id+" sequence. \""+j+"\" is not a valid amino acid. This sequence will be skipped. Please enter sequences with only 20 aa code. \n", file=sys.stderr)
+                    if(len(records))==1:
+                        error("Error. No valid sequence found")
+                    break
+            if seq_check == 1 and len_check ==1 :
+                sequences[i.id]=i.seq
+                print(i.id.ljust(maxlenname,' '),":",i.seq,"\n")
+        print("Number of valid sequences found:",len(sequences.keys()),"\n")
+        print("=================================================================\n")
+        return(sequences, maxlenname, maxlenseq)
+    except ValueError:
+        error("Error in reading the input fasta file. Please enter a valid fasta file")
+
+
+
 
 def precision_0(y_true, y_pred, labels=None, average='binary', sample_weight=None):
     '''
@@ -267,68 +333,77 @@ def f1_0(y_true, y_pred, labels=None, average='binary', sample_weight=None):
 
 
 
-
 def readmodel(mlfile):
     try:
-         print("Loading modelfile:", mlfile)
+         print("Loading modelfile:", mlfile,"\n")
          with open(mlfile, 'rb') as f:
              unpickler = MyCustomUnpickler(f)
              obj = unpickler.load()
              return obj
     except:
-        print("Error in reading model file")
-        sys.exit()
-    
-    
+        error("Error in reading model file")
 
-def combinefeature(pep, featurelist, vocab, aap_file, aat_file):
-    
-    a=np.empty([len(pep), 1])
-    if 'aap' in featurelist:
-        aapdic = readAAP("./aap/"+aap_file)
-        f_aap = np.array([aap(pep, aapdic, 1)]).T
-        a = np.column_stack((a,f_aap))
-        #print(f_aap)
-    if 'aat' in featurelist:
-        aatdic = readAAT("./aat/"+aat_file)
-        f_aat = np.array([aat(pep, aatdic, 1)]).T
-        a = np.column_stack((a, f_aat))
-        #print(f_aat)
-    if 'dpc' in featurelist:
-        f_dpc, name = DPC(pep)
-        # f_dpc = np.average(f_dpc, axis =1)
-        a = np.column_stack((a, np.array(f_dpc)))
-    if 'aac' in featurelist:
-        f_aac = AAC(pep)
-        a = np.column_stack((a, np.array(f_aac)))
-        #fname = fname + name
-    if 'paac' in featurelist:
-        f_paac = PAAC(pep)
-        f_paac = pca.fit_transform(f_paac)
-        a = np.column_stack((a, np.array(f_paac)))
-        #fname = fname + name
-    if 'kmer' in featurelist:
-        kmers = kmer(pep, 4, vocab=vocab, testing=1)
-        #f_kmer = np.array(kmers.X.toarray())
-        f_kmer = np.array(kmers.X.toarray())
-        a = np.column_stack((a, f_kmer))
-        #fname = fname + name
-    if 'ctd' in featurelist:
-        f_ctd, name = CTD(pep)
-        a = np.column_stack((a, np.array(f_ctd)))
-        #fname = fname + name
-    if 'protvec' in featurelist:
-            if os.path.isfile('./protvec/sp_sequences_4mers_vec.bin') == True:
-                f_protvec = np.array(protvec(pep, 4, './protvec/sp_sequences_4mers_vec.bin').embeddingX)
-                a = np.column_stack((a, f_protvec))
+
+def calcpeptides(seq, length, shortcheck, longcheck):  #return peptides of length 20 from the sequence
+    pep = []
+    peploc=[]
+    i=0
+    while i < len(seq):
+        if len(seq)<length:
+            shortcheck = 1
+            pep.append(seq.upper())
+            peploc.append(i+1)
+            break
+        if len(seq)>=length:
+            if i+length > len(seq):
+                break
             else:
-                print("Protvec binaries are missing. See README file.")
-                sys.exit()    
-                #print(f_protvec)
-    #print(a)
-    return a[:,1:]
+                pep.append(seq[i:i+length].upper())
+                peploc.append(i+1)
+                longcheck = 1
+            i = i + 1
+    #print(pep)
+    return pep, peploc, shortcheck, longcheck
+    
 
 
+def createpeptides(sequences, length, maxname, maxlen): #calculates petides for all the sequences
+    pep = []
+    peploc = []
+    seqid = []
+    print("Generating peptides of length "+str(length)+"\n")
+    global_longcheck = 0
+    global_shortcheck = 0
+    digit_len=len(str(maxlen))
+    digit_pepno=len(str((maxlen-length+1)))
+    for i in sequences.keys():
+        shortcheck = 0
+        longcheck = 0
+        peptemp, peploctemp, shortcheck, longcheck  = calcpeptides(str(sequences[i]), length, shortcheck, longcheck)
+        pep = pep + peptemp
+        peploc = peploc + peploctemp
+        seqid = seqid + [i]*len(peptemp)
+        seq_len = len(str(sequences[i]))
+        no_pep = len(peptemp)
+        if longcheck == 1 and shortcheck == 0:
+            print("sequence:",i.center(maxname,' '),"| length: "+"{:{align}{width}}".format(str(seq_len),align='>', width = digit_len),"| no. of peptides: "+"{:{align}{width}}".format(str(no_pep),align='<', width = digit_pepno)+"\n")
+            global_longcheck = 1
+        if shortcheck == 1:
+            print("sequence:",i.center(maxname,' '),"| length: "+"{:{align}{width}}".format(str(seq_len),align='>', width = digit_len),"| length is smaller than the specified peptide length of "+str(length)+"\n")
+            global_shortcheck = 1
+
+
+
+    print()
+    if global_longcheck == 0:
+        print("No sequences with length >",length,"found. Predictions will be made on the entire sequences\n")
+
+    if global_shortcheck == 1:
+        print("Note: If the length of a sequence is smaller than the specified length of the peptide (in this case: "+str(length)+"), the prediction will be made on the entire sequence.\n")
+    print("=================================================================\n")
+    return pep, peploc, seqid
+    
+    
 def plot(model, x,y):
     cv = StratifiedKFold(n_splits=5)
     splits = list(cv.split(x,y))
@@ -367,13 +442,15 @@ def predict(training_data, features):
     scaling = training_data['scaling']
     #plot(model , scaling.transform(training_features), np.array([1]*701 + [0]*701))
     features = scaling.transform(features)
+    return model.predict_proba(features)
     #print(model.score(scaling.transform(training_features),([1]*701)+([0]*701)))
-    try:
+    '''try:
         return model.predict_proba(features)
     except:
         print("Error in predicting epitopes.")
-        sys.exit()
+        sys.exit()'''
         
+
 def test(training_data, x_test, y_test):
     model = training_data['model'].best_estimator_
     scaling = training_data['scaling']
@@ -381,14 +458,16 @@ def test(training_data, x_test, y_test):
     print (model.score(features, y_test))
 
 
-def scoremodel(file, mlfile, aap_file, aat_file):
-    sequence = readseq(file)
-    pep = peptides(sequence)
+def scoremodel(file, mlfile, aap_file, aat_file, length):
+    sequences, maxname, maxlen = readseq(file, length)
+    pep, peploc, seqid = createpeptides(sequences, length, maxname, maxlen)
     training_data= readmodel(mlfile)
     #print(training_data.keys())
-    print ("Calculating the following features for test peptides...")
-    print(*training_data['featurelist'], sep=",")
+    print ("Calculating the",training_data['featurelist'],"features for the peptides. \n")
+    #print(*training_data['featurelist'], sep=",")
     features = combinefeature(pep, training_data['featurelist'], training_data['vocab'], aap_file, aat_file)
+    print("\nFeature calculation complete!\n")
+    print("=================================================================\n")
     '''newdata = open('abcpred-20.txt', 'r')
     anew = []
     for l in newdata.readlines():
@@ -403,12 +482,92 @@ def scoremodel(file, mlfile, aap_file, aat_file):
     x = combinefeature(testpeptides, training_data['featurelist'], training_data['vocab'])
     print(len(x),len(y_test))
     test(training_data, x, y_test)'''
-    return pep, predict(training_data, features)
+    print("Making predictions \n")
+    return sequences, pep, peploc, seqid, predict(training_data, features)
 
 
-if __name__ == "__main__":
-    peptide_list, pred_probability = scoremodel("./input/example.fasta", "./model/svm-model.pickle" )
-    print("List of predicted epitopes:")
-    for i in range(len(pred_probability)):
-        if pred_probability[i][1] >= 0.5:
-            print(peptide_list[i], pred_probability[i][1])
+def print_stdout(pep, peploc, seqid, pred, cutoff, length):
+    epitopelist = []
+    nonepitopelist = []
+    for i in range(len(pred)):
+        if pred[i][1] >= cutoff:
+            epitopelist.append(i)
+        if pred[i][1] < cutoff:
+            nonepitopelist.append(i)
+    
+    if len(epitopelist) > 0:
+        print("Peptides predicted as epitopes: \n")
+        #print("seq_id"+"\t"+"peptide"+"\t"+"start"+"\t"+"end"+"\t"+"score")
+        seqid_maxname = 0
+        pep_maxlen = 0
+        startloc_max = 0
+        for i in epitopelist:
+            if len(str(seqid[i])) > seqid_maxname:
+                seqid_maxname = len(str(seqid[i]))
+            if len(str(pep[i])) > pep_maxlen:
+                pep_maxlen = len(str(pep[i]))
+            if peploc[i] > startloc_max:
+                startloc_max = peploc[i]
+        width_seqid = 6
+        if seqid_maxname > 6:
+            width_seqid = seqid_maxname
+        width_pep = pep_maxlen
+        width_startloc = 5
+        if len(str(startloc_max)) > 5:
+            width_startloc = len(str(startloc_max))
+        width_endloc = 5
+        if len(str(startloc_max+length-1)) > 5:
+            width_endloc = len(str(startloc_max+length-1))
+        print("seq_id".rjust(width_seqid,' ')+"\t"+"peptide".center(width_pep,' ')+"\t"+"start".rjust(width_startloc,' ')+"\t"+"end".rjust(width_endloc,' ')+"\t"+"score".ljust(5,' '))
+        print("".rjust(width_seqid,'-')+"\t"+"".center(width_pep,'-')+"\t"+"".rjust(width_startloc,'-')+"\t"+"".rjust(width_endloc,'-')+"\t"+"".ljust(5,'-'))
+        for i in epitopelist:
+            print(str(seqid[i]).rjust(width_seqid,' ')+"\t"+str(pep[i]).center(width_pep,' ')+"\t"+str(peploc[i]).rjust(width_startloc,' ')+"\t"+str(peploc[i]+len(str(pep[i]))-1).rjust(width_endloc,' ')+"\t"+'{:{width}.{prec}f}'.format(pred[i][1], width = 5, prec = 3))
+        print()
+    else:
+        print("No peptides were predicted as epitopes \n")
+
+
+def print_file(sequences, pep, peploc, seqid, pred, cutoff, length, output):
+    #print("=================================================================\n")
+    #print("Writing to files.")
+    epitopelist = []
+    for i in range(len(pred)):
+        if pred[i][1] >= cutoff:
+            epitopelist.append(i)
+
+    seqid_maxname=0
+    for i in sequences.keys():
+            if len(str(i)) > seqid_maxname:
+                seqid_maxname = len(str(i))
+
+
+
+
+    if len(epitopelist) > 0:
+        output_string={}
+        for i in sequences.keys():
+            output_string[i]=['',0]
+        for i in epitopelist:
+            output_string[seqid[i]][0] = output_string[seqid[i]][0]+(str(seqid[i])+"\t"+str(pep[i])+"\t"+str(peploc[i])+"\t"+str(peploc[i]+len(str(pep[i]))-1)+"\t"+'{:{width}.{prec}f}'.format(pred[i][1], width = 5, prec = 3)+"\n")
+            output_string[seqid[i]][1] = output_string[seqid[i]][1] + 1
+    else:
+        print("None of the peptides were predicted as epitopes. \n")
+        sys.exit()
+
+    for i in output_string.keys():
+        with open(output+"/"+str(i)+".epitopes.txt", "w") as fileoutput:
+            if output_string[i][0] != '':
+                print("sequence:",i.center(seqid_maxname,' '),"| no. of peptides predicted as epitopes:",output_string[i][1])
+                print("writing predictions to:",output+"/"+str(i)+".epitopes.txt\n")
+                fileoutput.write("seq_id"+"\t"+"peptide"+"\t"+"start"+"\t"+"end"+"\t"+"score\n")
+                fileoutput.write(output_string[i][0])
+            else:
+                print("sequence:",i.center(seqid_maxname,' '),"| no. of peptides predicted as epitopes:",output_string[i][1])
+                print("writing predictions to:",output+"/"+str(i)+".epitopes.txt\n")
+                fileoutput.write("None of the peptides were predicted as epitopes.")
+
+
+
+
+
+
